862e88bc675b9727f0ce85ffb02512ee0601c071
name:"Dev <username:"devmeena7000@gmail.com>
2024-09-27 23:43:31 +0530

    basic react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='h-20 w-30 bg-red'>
+      <h1 className='bg-red text-2xl text-green-700' >Vite + React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
