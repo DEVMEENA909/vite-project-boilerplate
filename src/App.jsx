d32d0a653751ac0e5889a135b11ffed56cfaae56
name:"Dev <username:"devmeena7000@gmail.com>
2024-09-29 22:11:33 +0530

    form Handling

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,19 @@ function App() {
     setGender(e.target.value)
   }
   const submitHandler = (e) =>{
-   e.preventDefault()
-   console.log("submitted")
-   console.log(setGender(e))
+    e.preventDefault()
+    console.log("submitted")
+    console.log({user})
+    console.log({gender})
   }
   return (
     <>
     <form onSubmit={submitHandler} encType='multipart/form-data' className='p-10 text-2xl flex gap-2 items-center' >
       <input type="file" name="avatar" onChange={handleFileChange} />
       <input  className='border-2 p-4 rounded-md' name='user' onChange={(e)=> setUser(e.target.value)} type="text" value={user} placeholder='username' />
-      <input type="radio" name="gender" defaultValue="others" checked={gender ==="others"} onChange={selectGender} /> others
-      <input type="radio" name="gender" defaultValue="Male" checked={gender ==="Male" } onChange={selectGender} /> Male
-      <input type="radio" name="gender" defaultValue="Female" checked={gender ==="Female" } onChange={(e)=> setGender(e.target.value)} /> Female
+      <input type="radio" name="gender" defaultValue="others" checked={gender ==="others"} onChange={(e) =>setGender(e.target.value)} /> others
+      <input type="radio" name="gender" defaultValue="Male" checked={gender ==="Male" } onChange={(e) =>setGender(e.target.value)} /> Male
+      <input type="radio" name="gender" defaultValue="Female" checked={gender ==="Female" } onChange={(e) =>setGender(e.target.value)} /> Female
       <button className='border-2 w-fit p-4 rounded-md'>Submit</button>
     </form>
     </>
